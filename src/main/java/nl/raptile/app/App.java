184d3417cff159a43459cca27513ec5aaa96d9b6
Wesley van den Braak <wvdbraak@bkwi.nl>
2017-02-17 10:52:45 +0100

    remove projectNamed dir
    remove projectNamed dir and move files to project dir

---
+++ src/main/java/nl/raptile/app/App.java
@@ -0,0 +1,13 @@
+package nl.raptile.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
